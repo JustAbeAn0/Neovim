0dd5a595b173b0af9ea72ec48cbd63c4165f9b89
Beans <srijan.mondal78@gmail.com>
2025-07-27 13:19:00 +0530

    updates

--- lua/Srijan/plugins/lualine.lua
+++ lua/Srijan/plugins/lualine.lua
@@ -3,7 +3,8 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     local lualine = require("lualine")
-    local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+    local lazy_status = require("lazy.status")
+    local devicons = require("nvim-web-devicons")
 
     local colors = {
       blue = "#65D1FF",
@@ -43,60 +44,79 @@ return {
         c = { bg = colors.bg, fg = colors.fg },
       },
       inactive = {
-        a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
-        b = { bg = colors.inactive_bg, fg = colors.semilightgray },
-        c = { bg = colors.inactive_bg, fg = colors.semilightgray },
+        a = { bg = colors.inactive_bg, fg = colors.fg, gui = "bold" },
+        b = { bg = colors.inactive_bg, fg = colors.fg },
+        c = { bg = colors.inactive_bg, fg = colors.fg },
       },
     }
 
-    -- configure lualine with modified theme
-lualine.setup({
-  options = {
-    theme = my_lualine_theme,
-    section_separators = '',
-    component_separators = '',
-  },
-  sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {
-      {
-        lazy_status.updates,
-        cond = lazy_status.has_updates,
-        color = { fg = "#ff9e64" },
-      },
-      { "encoding" },
-      { "fileformat" },
-      { "filetype" },
-    },
-    lualine_y = {},
-    lualine_z = {},
-  },
-  tabline = {
-      lualine_a = {
-    {
-      function()
-        for _, buf in ipairs(vim.fn.getbufinfo({ buflisted = 1 })) do
-          local name = buf.name
-          if name ~= "" and vim.fn.filereadable(name) == 1 then
-            local filename = vim.fn.fnamemodify(name, ":t")
-            local extension = vim.fn.fnamemodify(name, ":e")
-            local icon = require("nvim-web-devicons").get_icon(filename, extension, { default = true })
-            return icon .. " " .. filename
-          end
+    local last_file_bufnr = nil
+    vim.api.nvim_create_autocmd("BufEnter", {
+      callback = function(args)
+        local buftype = vim.api.nvim_buf_get_option(args.buf, "buftype")
+        local bufname = vim.api.nvim_buf_get_name(args.buf)
+        local filetype = vim.api.nvim_buf_get_option(args.buf, "filetype")
+        
+        -- Only track actual file buffers, not terminals or special buffers
+        if buftype == "" and bufname ~= "" and not bufname:match("term://") and filetype ~= "terminal" then
+          last_file_bufnr = args.buf
         end
-        return "📄 [No File]"
       end,
-    },
-  },
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = {},
-  },
-  extensions = {},
-})
+    })
+
+    local function show_file_in_tab()
+      -- Always use the last file buffer, don't fall back to current buffer
+      if not last_file_bufnr or not vim.api.nvim_buf_is_valid(last_file_bufnr) then
+        return " [No File]"
+      end
+
+      local name = vim.api.nvim_buf_get_name(last_file_bufnr)
+
+      if name == "" then
+        return " [No File]"
+      end
+
+      local filename = vim.fn.fnamemodify(name, ":t")
+      local ext = vim.fn.fnamemodify(name, ":e")
+      local icon, _ = devicons.get_icon(filename, ext, { default = true })
+      return icon .. " " .. filename
+    end
+
+    lualine.setup({
+      options = {
+        theme = my_lualine_theme,
+        section_separators = '',
+        component_separators = '',
+      },
+      sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {
+          {
+            lazy_status.updates,
+            cond = lazy_status.has_updates,
+            color = { fg = "#ff9e64" },
+          },
+          { "encoding" },
+          { "fileformat" },
+          { "filetype" },
+        },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      tabline = {
+        lualine_a = {
+          { show_file_in_tab },
+        },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+      },
+      extensions = {},
+    })
   end,
 }
+
